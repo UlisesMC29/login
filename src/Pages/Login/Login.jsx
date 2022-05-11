af592a115c6fa65bb25268b8c763be40e5dd2415
UlisesMC29 <contac_umc@hotmail.com>
2022-05-11 16:11:15 -0500

    Resgiter Modified and validation added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -8,29 +8,60 @@ import "../Login/Login.css";
 
 export const Login = () => {
 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState(null)
+
+    const procesarDatos = (e) => {
+        e.preventDefault()
+        if (!email.trim()) {
+            // console.log('Ingrese el email')
+            setError('Ingresar un email para continuar')
+            return
+        }
+
+        if (!pass.trim()) {
+            // console.log('Ingrese una contraseña')
+            setError('Ingrese una contraseña para continuar')
+            return
+        }
+
+        setError(null)
+    }
 
     return (
 
-        <div className="login-container">
-            {/* <Navbar />   ELIMINADO PORQUE LA NAVBAR ESTA ASIGNADA EN APP.JS*/}
+        <form onSubmit={procesarDatos}>
 
-            <h1>Login</h1>
-            <div className='input-container'>
-                <label>Email</label>
-                <input type="text" name="" id="" placeholder="example@mail.com" onChange={e => setEmail(e.target.value)} value={email} />
-            </div>
-            <div className='input-container'>
-                <label>Password</label>
-                <input type="password" name="" id="" placeholder="*****" onChange={e => setPass(e.target.value)} value={pass} />
-            </div>
-            <Link to="/" style={{ textDecoration: 'none' }}><button >Login</button></Link>
-            <div className="fnText-container">
-                <p>Not register yet? <Link to='/Register' style={{ textDecoration: 'none' }}><a>Register</a></Link> </p>
-            </div>
+            <div className="login-container">
+                {/* <Navbar />   ELIMINADO PORQUE LA NAVBAR ESTA ASIGNADA EN APP.JS*/}
 
-        </div >
+                <h1>Login</h1>
+                {
+                    error && (
+                        <div className="alert-error">
+                            {error}
+                            <br />
+                        </div>
+                    )
+                }
+                <div className='input-container'>
+                    <label>Email</label>
+                    <input type="text" name="" id="" placeholder="example@mail.com" onChange={e => setEmail(e.target.value)} value={email} />
+                </div>
+                <div className='input-container'>
+                    <label>Password</label>
+                    <input type="password" name="" id="" placeholder="*****" onChange={e => setPass(e.target.value)} value={pass} />
+                </div>
+                {/* <Link to="/" style={{ textDecoration: 'none' }}><button >Login</button></Link> */}
+                <button >Login</button>
+                <div className="fnText-container">
+                    <p>Not register yet? <Link to='/Register' style={{ textDecoration: 'none' }}><a>Register</a></Link> </p>
+                </div>
+
+            </div >
+
+        </form>
 
     );
 }
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Navbar } from "../../components/NavBar/navbar";
 import "./register.css";
 import { Link } from "react-router-dom";
 
 
 export const Register = () => {
+
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState(null)
+
     return (
         <div>
             {/* <Navbar /> */}
             <div className="register-container">
                 <h1>REGISTER</h1>
                 <div className="input-container">
-                    <label>Name</label>
-                    <input type="text" placeholder="Fausto Example" />
                     <label>Email</label>
-                    <input type="text" placeholder="example@email.com" />
+                    <input type="text" placeholder="example@email.com" onChange={e => setEmail(e.target.value)} value={email} />
                     <label>Password</label>
-                    <input type="text" placeholder="*******" />
+                    <input type="password" placeholder="*******" onChange={e => setPass(e.target.value)} value={pass} />
                 </div>
                 <Link to="/Login" style={{ textDecoration: 'none' }}><button >Register</button></Link>
                 <p>Already member? <Link to='/Login' style={{ textDecoration: 'none' }}><a>Login</a></Link> </p>
